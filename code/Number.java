4a78a4ccebabf437f152593856eacf1828ac4031
JoannaKl <joannakl@cs.nyu.edu>
2018-01-29 07:51:44 -0500

    week 2 updates, project 1 posted

---
+++ code/Number.java
@@ -0,0 +1,49 @@
+
+public class Number {
+	private int num;
+	
+	public Number ( ) {
+		//TODO: needs to be implemented 
+	}
+
+	public Number ( int num ) {
+		//TODO: needs to be implemented 
+	}
+
+	public void add ( Number n )  {
+		//TODO: needs to be implemented 
+	}
+	
+	public void subtract ( Number n ) {
+		//TODO: needs to be implemented 
+	}
+	
+	public void multiply ( Number n ) {
+		//TODO: needs to be implemented 
+	}
+	
+	public void divide ( Number n ) {
+		//TODO: needs to be implemented 
+	}
+	
+	public Number duplicate ( ) {
+		//TODO: needs to be implemented 
+		return null; 
+	}
+	
+	public int getNumber () { 
+		return num; 
+	}
+	
+	public String toString ( ) {
+		//TODO: needs to be implemented 
+		return null; 
+	}
+	
+	@Override
+	public boolean equals (Object o ) {
+		//TODO: needs to be implemented 
+		return false;
+	}
+	
+}
